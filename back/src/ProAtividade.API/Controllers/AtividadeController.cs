43a6db1c9cb478da5c321a404e15152c0c6fd2f6
netoschmitt <agneloschmittneto@gmail.com>
2023-05-02 11:38:38 -0300

    Integração EFCore

--- back/src/ProAtividade.API/Controllers/AtividadeController.cs
+++ back/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Data;
 using ProAtividade.API.Models;
 
 namespace ProAtividade.API.Controllers
@@ -11,43 +12,59 @@ namespace ProAtividade.API.Controllers
     [Route("api/[controller]")]
     public class AtividadeController : ControllerBase
     {
-        public IEnumerable<Atividade> Atividades = new List<Atividade>()
-            {
-                new Atividade(1),
-                new Atividade(2),
-                new Atividade(3)
-            };
+        private readonly DataContext _context;
+        public AtividadeController(DataContext context)
+        {
+           _context = context;
+            
+        }
 
         [HttpGet]
         public IEnumerable<Atividade> Get()
         {
-            return Atividades; 
-        
+            return _context.Atividades;
         }
+
         [HttpGet("{id}")]
         public Atividade GetById(int id)
         {
-            return Atividades.FirstOrDefault(ativ => ativ.Id == id);
+            return _context.Atividades.FirstOrDefault(ativ => ativ.Id == id);
         }
 
         [HttpPost]
         public IEnumerable<Atividade> Post(Atividade atividade)
         {
-            return Atividades.Append<Atividade>(atividade);
+            _context.Atividades.Add(atividade);
+            if (_context.SaveChanges()> 0)
+                return _context.Atividades;
+            else 
+                throw new Exception("Não foi possivel adicionar uma atividade!");
         }
 
         
         [HttpPut("{id}")]
-        public Atividade PutById(int id, Atividade atividade)
+        public Atividade Put(int id, Atividade atividade)
         {
-            atividade.Id = atividade.Id + 1;
-            return atividade;
+           if(atividade.Id != id) throw new Exception("Você esta tentando atualizar a atividade errada");
+
+           _context.Update(atividade);
+           
+           if (_context.SaveChanges() > 0)
+                return _context.Atividades.FirstOrDefault(ativ => ativ.Id == id);
+            else
+                return new Atividade();
+
         }
 
         [HttpDelete("{id}")]
-        public string DeleteById(int id)
+        public bool Delete(int id)
         {
-            return $"meu primeiro metodo Delete com parametro{id}";
+            var atividade = _context.Atividades.FirstOrDefault(ativ => ativ.Id == id);
+            if(atividade == null)
+                throw new Exception("Você está tentando deletar uma atividade não existente");
+
+            _context.Remove(atividade);
+            return _context.SaveChanges() > 0;
         }
 
     }
---
+++ back/src/ProAtividade.API/Models/Prioridade.cs
@@ -0,0 +1,11 @@
+namespace ProAtividade.API.Models
+{
+    public enum Prioridade
+    {
+        NaoDefinido,
+        Baixa,
+        Normal,
+        Alta
+
+    }
+}
